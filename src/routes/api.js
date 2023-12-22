674ac06cb084c8261f13f297e6b4eb8dbe471803
chndrraa <154586958+chndrraa@users.noreply.github.com>
2023-12-22 16:34:14 +0700

    Add files via upload

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const kategoriController = require('../controllers/kategori');
+const itemController = require('../controllers/item');
+const route = express.Router();
 
+route.get('/kategori', kategoriController.kategori);
+route.get('/item/deskripsi/:idItem', itemController.deskripsi);
+route.get('/item/:idKategori', itemController.item);
+module.exports = route;
